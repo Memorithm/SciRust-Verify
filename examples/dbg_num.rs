c9426ed8435f08e1cea149ab40f6cd2042dcf032
MEMOPERF <contact@checkupauto.fr>
2026-08-25 14:36:11 +0200

    chore: remove stray debug example

--- examples/dbg_num.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    use scirust_verify_numerics::*;
-    let stdout = "SCIRUST_VERIFY_OBS_V1 {\"kind\":\"metric\",\"name\":\"latency\",\"value\":1.5}\n";
-    match parse_observations(stdout) {
-        Ok(o) => println!("OK {:?}", o),
-        Err(e) => println!("ERR {:?}", e),
-    }
-}
